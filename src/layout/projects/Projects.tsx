c2bb6fe7ccfa123914dc566bd8b21aff4ae3c7cb
Daria Kot <poturayko@gmail.com>
2024-12-09 17:24:16 +0100

    Added props to Project

--- src/layout/projects/Projects.tsx
+++ src/layout/projects/Projects.tsx
@@ -1,14 +1,17 @@
 import { BlockTitle } from "../../components/title/BlockTitle";
-import { Project } from "../../components/project/Project";
+import { Project } from "./project/Project";
+import Proj1Img from "../../assets/images/photo/project1.jpeg";
+import Proj2Img from "../../assets/images/photo/project2.jpeg";
+import Proj3Img from "../../assets/images/photo/project3.jpeg";
 
 
 export const Projects = () => {
     return (
         <>
         <BlockTitle title="Projects"/>
-        <Project/>
-        <Project/>
-        <Project/>
+            <Project title={"Project 1"} text={"I created this personal project in order to show how to create an interface in Figma using a portfolio as an example."} src={Proj1Img}/>
+            <Project title={"Project 2"} text={"I created this personal project in order to show how to create an interface in Figma using a portfolio as an example."} src={Proj2Img}/>
+            <Project title={"Project 3"} text={"I created this personal project in order to show how to create an interface in Figma using a portfolio as an example."} src={Proj3Img}/>
         </>
     );
 };
---
+++ src/layout/projects/project/Project.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { FlexWrapper } from "../../../components/wrapper/FlexWrapper";
+
+type ProjectPropsType = {
+    title: string;
+    text: string;
+    src: string;
+
+}
+
+export const Project = (props: ProjectPropsType) => {
+    return (
+        <FlexWrapper>
+            <div>
+                <Title>{props.title}</Title>
+                <Text>{props.text}</Text>
+                <Link>View Project</Link>
+            </div>
+            <Photo src={props.src} alt="Project cover" />
+        </FlexWrapper>
+
+    );
+};
+
+const Title = styled.h3`
+    
+`;
+ 
+const Text = styled.p`
+
+`;
+
+const Link = styled.button`
+
+`;
+
+const Photo = styled.img`
+max-width: 300px;
+`;
